f3f0d145b5906b68eadfdedc6e9ae0866d019fd3
David-vst <awbjcj@gmail.com>
2026-07-23 16:23:30 -0400

    feat: update entrypoint script to include enterprise URL for auth commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+ENTERPRISE_URL="https://aptv.ghe.com/"
 if [ "$1" = "--auth" ]; then
   # Run auth command
-  exec bun --use-system-ca run dist/main.js auth
+  exec bun --use-system-ca run dist/main.js auth --enterprise-url="$ENTERPRISE_URL"
 else
   # Default command
-  exec bun --use-system-ca run dist/main.js start -g "$GH_TOKEN" "$@"
+  exec bun --use-system-ca run dist/main.js start -g "$GH_TOKEN" --enterprise-url="$ENTERPRISE_URL" "$@"
 fi
 
